a53decb0d14026ce3128bf651c7a656f23b6f035
yazhuy <85384621+yazhuy@users.noreply.github.com>
2023-05-03 03:11:04 +0000

    Update server.js

--- files/server.js
+++ files/server.js
@@ -26,16 +26,16 @@ app.get("/", function (req, res) {
 // });
 
 //获取系统进程表
-app.get("/status", function (req, res) {
-  let cmdStr = "pm2 list; ps -ef";
-  exec(cmdStr, function (err, stdout, stderr) {
-    if (err) {
-      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-    } else {
-      res.type("html").send("<pre>获取守护进程和系统进程表：\n" + stdout + "</pre>");
-    }
-  });
-});
+//app.get("/status", function (req, res) {
+//  let cmdStr = "pm2 list; ps -ef";
+//  exec(cmdStr, function (err, stdout, stderr) {
+//    if (err) {
+//      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+//    } else {
+//      res.type("html").send("<pre>获取守护进程和系统进程表：\n" + stdout + "</pre>");
+//    }
+//  });
+//});
 
 //获取系统监听端口
 app.get("/listen", function (req, res) {
@@ -50,17 +50,17 @@ app.get("/listen", function (req, res) {
   });
 
 //获取节点数据
-app.get("/list", function (req, res) {
-    let cmdStr = "bash argo.sh";
-    exec(cmdStr, function (err, stdout, stderr) {
-      if (err) {
-        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-      }
-      else {
-        res.type("html").send("<pre>节点数据：\n\n" + stdout + "</pre>");
-      }
-    });
-  });
+//app.get("/list", function (req, res) {
+//    let cmdStr = "bash argo.sh";
+//    exec(cmdStr, function (err, stdout, stderr) {
+//      if (err) {
+//        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+//      }
+//      else {
+//        res.type("html").send("<pre>节点数据：\n\n" + stdout + "</pre>");
+//      }
+//    });
+//  });
 
 //获取系统版本、内存信息
 app.get("/info", function (req, res) {
